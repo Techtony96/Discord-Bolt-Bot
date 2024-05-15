2fc8f992faccdb2c51af723eb7291659d4afdeb7
Tony Pappas <techtony96@gmail.com>
2024-05-15 07:58:18 -0600

    Temporary permission fix for count command

--- src/main/java/net/ajpappas/discord/boltbot/modules/counting/CountCommand.java
+++ src/main/java/net/ajpappas/discord/boltbot/modules/counting/CountCommand.java
@@ -5,16 +5,15 @@ import discord4j.core.event.domain.interaction.ApplicationCommandInteractionEven
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.discordjson.json.ApplicationCommandRequest;
 import discord4j.discordjson.json.ImmutableApplicationCommandRequest;
-import net.ajpappas.discord.common.command.GuildSlashCommand;
+import discord4j.rest.util.Permission;
+import discord4j.rest.util.PermissionSet;
+import net.ajpappas.discord.common.command.GlobalSlashCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-import java.util.Collection;
-import java.util.Collections;
-
 @Component
-public class CountCommand implements GuildSlashCommand {
+public class CountCommand implements GlobalSlashCommand {
 
     private final CountingListener countingListener;
 
@@ -31,6 +30,11 @@ public class CountCommand implements GuildSlashCommand {
     }
 
     @Override
+    public PermissionSet requiredPermissions() {
+        return PermissionSet.of(Permission.SEND_MESSAGES); // Temporary fix because none() results in Discord requiring admin
+    }
+
+    @Override
     public Mono<Void> handle(ApplicationCommandInteractionEvent event) {
         Snowflake channelId = event.getInteraction().getChannelId();
         long count = countingListener.getCurrentCountMap().getOrDefault(channelId, new CountingData()).getCount();
@@ -42,8 +46,4 @@ public class CountCommand implements GuildSlashCommand {
         return ChatInputInteractionEvent.class;
     }
 
-    @Override
-    public Collection<Long> guilds() {
-        return Collections.singleton(110927581070512128L);
-    }
 }
